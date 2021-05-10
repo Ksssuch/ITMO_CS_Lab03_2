3b44e2ae2981a76f77e8fe4ef248a3e5accce00d
Ksssuch <ksssuch@mail.ru>
2021-05-10 23:07:58 +0300

    Lab03_2

---
+++ WhatDay (2).cs
@@ -0,0 +1,277 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace WhatDay2
+{
+        enum MonthName
+        {
+            January,
+            February,
+            March,
+            April,
+            May,
+            June,
+            July,
+            August,
+            September,
+            October,
+            November,
+            December
+        }
+
+        class WhatDay
+        {
+            static void Main(string[] args)
+            {
+                //
+                // Add code here
+                //
+                Console.WriteLine("Enter year: ");
+                int year = int.Parse(Console.ReadLine());
+
+                int monthNum = 0;
+
+
+
+                if (year < 1)
+                {
+                    throw new ArgumentOutOfRangeException("Incorrect Year");
+                }
+
+                bool isLeapYear = (year % 4 == 0) && (year % 100 != 0 || year % 400 == 0);
+
+                int dayNumUpperBound;
+
+                if (isLeapYear)
+                {
+                    Console.WriteLine(" IS a leap year");
+                }
+                else
+                {
+                    Console.WriteLine(" is NOT a leap year");
+                }
+
+                if (isLeapYear)
+                {
+                    dayNumUpperBound = 366;
+                }
+                else
+                {
+                    dayNumUpperBound = 365;
+                }
+
+                Console.Write("Please enter a day number between 1 and {0}: ", dayNumUpperBound);
+                int dayNum = int.Parse(Console.ReadLine());
+
+                if (dayNum < 1 || dayNum > dayNumUpperBound)
+                {
+                    throw new ArgumentOutOfRangeException("Day out of range");
+                }
+
+                int[] DaysInMonths = new int[12];
+                for (int i = 0; i < 12; i++)
+                {
+                    DaysInMonths[i] = DateTime.DaysInMonth(year, i + 1);
+                }
+
+
+
+
+                foreach (int daysInMonth in DaysInMonths)
+                {
+                    if (dayNum <= daysInMonth)
+                    {
+                        break;
+                    }
+                    else
+                    {
+                        dayNum -= daysInMonth;
+                        monthNum++;
+                    }
+                }
+
+
+            ////////////////////////////////////////////////////////////////////
+            ////// Uncomment the code below  before Calculating           //////
+            /////  the month and day pair from a day number               //////
+            ////////////////////////////////////////////////////////////////////
+
+            //if (dayNum <= 31)
+            //{ // January
+            //    goto End;
+            //}
+            //else
+            //{
+            //    dayNum -= 31;
+            //    monthNum++;
+            //}
+
+            //if (dayNum <= 28)
+            //{ // February
+            //    goto End;
+            //}
+            //else
+            //{
+            //    dayNum -= 28;
+            //    monthNum++;
+            //}
+
+            //if (dayNum <= 31)
+            //{ // March
+            //    goto End;
+            //}
+            //else
+            //{
+            //    dayNum -= 31;
+            //    monthNum++;
+            //}
+
+            //if (dayNum <= 30)
+            //{ // April
+            //    goto End;
+            //}
+            //else
+            //{
+            //    dayNum -= 30;
+            //    monthNum++;
+            //}
+
+            //if (dayNum <= 31)
+            //{ // May
+            //    goto End;
+            //}
+            //else
+            //{
+            //    dayNum -= 31;
+            //    monthNum++;
+            //}
+
+
+            //if (dayNum <= 30)
+            //{ // June
+            //    goto End;
+            //}
+            //else
+            //{
+            //    dayNum -= 30;
+            //    monthNum++;
+            //}
+
+            //if (dayNum <= 31)
+            //{ // July
+            //    goto End;
+            //}
+            //else
+            //{
+            //    dayNum -= 31;
+            //    monthNum++;
+            //}
+
+            //if (dayNum <= 31)
+            //{ // August
+            //    goto End;
+            //}
+            //else
+            //{
+            //    dayNum -= 31;
+            //    monthNum++;
+            //}
+
+            //if (dayNum <= 30)
+            //{ // September
+            //    goto End;
+            //}
+            //else
+            //{
+            //    dayNum -= 30;
+            //    monthNum++;
+            //}
+
+            //if (dayNum <= 31)
+            //{ // October
+            //    goto End;
+            //}
+            //else
+            //{
+            //    dayNum -= 31;
+            //    monthNum++;
+            //}
+
+            ////////////////////////////////////////////////////////////////
+            //// TODO: Implement if statements for November and December ///
+            ////////////////////////////////////////////////////////////////
+            //if (dayNum <= 30)
+            //{ // November
+            //    goto End;
+            //}
+            //else
+            //{
+            //    dayNum -= 31;
+            //    monthNum++;
+            //}
+
+            //if (dayNum <= 31)
+            //{ // December
+            //    goto End;
+            //}
+            //else
+            //{
+            //    dayNum -= 31;
+            //    monthNum++;
+            //}
+
+            //////////////////////////////////////////////////////////////
+            // TODO:												  ////
+            //		Add an identifer label End below and              ////
+            //		declare string variable							  ////
+            //////////////////////////////////////////////////////////////
+            End:
+                string monthName;
+
+                //switch (monthNum)
+                //{
+                //    case 0:
+                //        monthName = "January"; break;
+                //    case 1:
+                //        monthName = "February"; break;
+                //    case 2:
+                //        monthName = "March"; break;
+                //    case 3:
+                //        monthName = "April"; break;
+                //    case 4:
+                //        monthName = "May"; break;
+                //    case 5:
+                //        monthName = "June"; break;
+                //    case 6:
+                //        monthName = "July"; break;
+                //    case 7:
+                //        monthName = "August"; break;
+                //    case 8:
+                //        monthName = "September"; break;
+                //    case 9:
+                //        monthName = "October"; break;
+                //    case 10:
+                //        monthName = "November"; break;
+                //    case 11:
+                //        monthName = "December"; break;
+                //    default:
+                //        monthName = "not done yet"; break;
+
+                //        //////////////////////////////////////////////////////////////////
+                //        // TODO: Implement the case 10 and case 11 for November         //
+                //        //       and December											//
+                //        //       Add a default label									//
+                //        //////////////////////////////////////////////////////////////////
+
+                //}
+                monthName = ((MonthName)monthNum).ToString();
+                Console.WriteLine("{0} {1}", dayNum, monthName);
+            }
+            // Don't modify anything below here
+            static System.Collections.ICollection DaysInMonths
+                = new int[12] { 31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31 };
+
+        }
+    }
